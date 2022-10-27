ad6aad9838e5e2b745c5c22cd54b5f782101b35d
Comaneci Christie <ccomaneci@gmail.com  git config --global user.name Comanecigit config --user.email ccomaneci@gmail.com>
2022-10-26 23:49:33 -0400

    updating pewlett-hackard-analysis

--- Queries/Employee_Database_Challenge.sql.sql
+++ Queries/Employee_Database_Challenge.sql.sql
@@ -1,10 +1,52 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Deliverable 1
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+select e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date, 
+ti.to_date,
+e.birth_date
+into retirement_titles
+FROM employees as e
+inner join titles as ti
+on (e.emp_no = ti.emp_no)
+where (e.birth_date between '1952-01-01' and '1955-12-31')
+order by e.emp_no;
+
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+where to_date = '9999-01-01'
+ORDER BY emp_no, title DESC;
+
+
+select count(*) as count, title
+into retiring_titles
+from unique_titles
+group by title
+order by count(*) desc;
+
+
+-- Deliverable 2
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+into mentorship_eligibilty
+from employees as e
+inner join dept_emp as de
+on (e.emp_no = de.emp_no)
+inner join titles as ti
+on (e.emp_no = ti.emp_no)
+where(e.birth_date between '1965-01-01' and '1965-12-31')
+and de.to_date = '9999-01-01'
+order by e.emp_no;
